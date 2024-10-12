1f0e5480af8ae6dc4236a109380524c4ee6968b2
Dzanija Ivan <ivan.dzanija222@gmail.com>
2024-10-12 16:55:00 +0200

    Template

---
+++ CSES/problemSet/Sort&Search/josephus_problem_2.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <ios>
+#include <iostream>
+#include <vector>
+
+using ll = int64_t;
+
+int main(void) {
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	std::cout.tie(0);
+	int n, k;
+	std::cin >> n >> k;
+
+	std::vector<std::vector<ll>> d2;
+
+	return 0;
+}
---
+++ cp_template.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <deque>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <utility>
+#include <vector>
+#define INF (int)1e9
+
+using ll = int64_t;
+using namespace std;
+using namespace __gnu_pbds;
+
+int main(void) {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	std::cout << 0 << std::endl;
+
+	return 0;
+}
