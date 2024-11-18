fcd5be4ed5db3b373c399ee697c9cd3f042aefc5
Dzanija Ivan <ivan.dzanija222@gmail.com>
2024-11-18 04:54:33 +0100

    Solved

--- CSES/problemSet/Sort&Search/maximum_subarray_sum_2.cpp
+++ CSES/problemSet/Sort&Search/maximum_subarray_sum_2.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <climits>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
@@ -17,6 +18,27 @@ using indexed_set = tree<T, null_type, std::less<T>, rb_tree_tag,
 int count_bits(ll number) { return (int)log2(number) + 1; }
 int main(void) {
 	std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
-
+	ll n, l, r;
+	cin >> n >> l >> r;
+	vector<ll> ps(n + 1, 0);
+	vector<ll> b(n, 0);
+	ll ans = LLONG_MIN;
+	for (ll i = 0; i < n; ++i) {
+		cin >> b[i];
+		ps[i + 1] = ps[i] + b[i];
+	}
+	multiset<ll> s;
+	for (ll i = l; i <= r; ++i) {
+		s.insert(ps[i]);
+	}
+	for (ll i = 1; i <= n && !s.empty(); ++i) {
+		ll d = ps[i - 1];
+		ans = max(ans, *(prev(s.end()))-d);
+		s.erase(s.lower_bound(ps[l + i - 1]));
+		if (i + r <= n) {
+			s.insert(ps[i + r]);
+		}
+	}
+	cout << ans << endl;
 	return 0;
 }
