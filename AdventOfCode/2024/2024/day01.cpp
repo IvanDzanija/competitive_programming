c8df35f5541333937b39bde2291dd3f6bf57943c
Dzanija Ivan <ivan.dzanija222@gmail.com>
2024-12-01 15:40:28 +0100

    Solved

---
+++ AdventOfCode/2024/2024/day01.cpp
@@ -0,0 +1,8 @@
+//
+//  day01.cpp
+//  2024
+//
+//  Created by Ivan Dzanija on 01.12.2024..
+//
+
+#include "day01.hpp"
---
+++ AdventOfCode/2024/2024/day01.hpp
@@ -0,0 +1,13 @@
+//
+//  day01.hpp
+//  2024
+//
+//  Created by Ivan Dzanija on 01.12.2024..
+//
+
+#ifndef day01_hpp
+#define day01_hpp
+
+#include <stdio.h>
+
+#endif /* day01_hpp */
