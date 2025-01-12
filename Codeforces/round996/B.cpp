3d344839903686b3f9c5a05d149c560e3543a5b8
Dzanija Ivan <ivan.dzanija222@gmail.com>
2025-01-12 17:35:07 +0100

    solved

---
+++ Codeforces/round996/B.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using ll = int64_t;
+using ull = uint64_t;
+using ld = long double;
+#define INF (int)1e9
+#define all(x) (x).begin(), (x).end()
+#define MOD (int)(1e9 + 7)
+
+using namespace __gnu_pbds;
+using namespace std;
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+template <typename T>
+using indexed_set = tree<T, null_type, std::less<T>, rb_tree_tag,
+						 tree_order_statistics_node_update>;
+struct custom_hash {
+	static uint64_t splitmix64(uint64_t x) {
+		// http://xorshift.di.unimi.it/splitmix64.c
+		x += 0x9e3779b97f4a7c15;
+		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+		return x ^ (x >> 31);
+	}
+
+	size_t operator()(uint64_t x) const {
+		static const uint64_t FIXED_RANDOM =
+			chrono::steady_clock::now().time_since_epoch().count();
+		return splitmix64(x + FIXED_RANDOM);
+	}
+};
+void solve() {
+	int n;
+	cin >> n;
+	vector<ll> a(n, 0);
+	vector<ll> b(n, 0);
+	vector<ll> c(n, 0);
+	ll mn = INF;
+	ll mx = 0;
+	int f = 0;
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < n; ++i) {
+		cin >> b[i];
+		if (a[i] - b[i] >= 0 && a[i] - b[i] < mn) {
+			mn = a[i] - b[i];
+		}
+		if (a[i] - b[i] < 0) {
+			++f;
+			mx += b[i] - a[i];
+		}
+	}
+	if (f == 0) {
+		cout << "YES" << endl;
+		return;
+	} else if (f > 1) {
+		cout << "NO" << endl;
+		return;
+	} else {
+		if (mx <= mn) {
+			cout << "YES" << endl;
+		} else {
+			cout << "NO" << endl;
+		}
+		return;
+	}
+
+	return;
+}
+
+signed main(void) {
+	std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+#ifdef LOCAL
+		cout << "--------------------------------------------" << endl;
+#endif
+	}
+
+#ifdef LOCAL
+	cerr << "Finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+	return 0;
+}
