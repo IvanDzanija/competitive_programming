cfda2bde02eba8b619761aa83bc896ff5a592068
Dzanija Ivan <ivan.dzanija222@gmail.com>
2025-01-13 04:05:46 +0100

    solved

---
+++ Codeforces/additional/2053C.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using ll = int64_t;
+using ull = uint64_t;
+using ld = long double;
+#define INF (int)1e9
+#define all(x) (x).begin(), (x).end()
+#define MOD (int)(1e9 + 7)
+
+using namespace __gnu_pbds;
+using namespace std;
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+template <typename T>
+using indexed_set = tree<T, null_type, std::less<T>, rb_tree_tag,
+						 tree_order_statistics_node_update>;
+struct custom_hash {
+	static uint64_t splitmix64(uint64_t x) {
+		// http://xorshift.di.unimi.it/splitmix64.c
+		x += 0x9e3779b97f4a7c15;
+		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+		return x ^ (x >> 31);
+	}
+
+	size_t operator()(uint64_t x) const {
+		static const uint64_t FIXED_RANDOM =
+			chrono::steady_clock::now().time_since_epoch().count();
+		return splitmix64(x + FIXED_RANDOM);
+	}
+};
+ll res = 0;
+ll k;
+vector<ll> vec;
+void rec(ll r, ll it) {
+	if (r < k) {
+		return;
+	}
+	ll m = (1 + r) / 2;
+	if (r & 1) {
+		res += (1 << it);
+		rec(m - 1, it + 1);
+	} else {
+		rec(m, it + 1);
+	}
+}
+ll rev(ll x) {
+	ll res = 0;
+	while (x > 0) {
+		res <<= 1;
+		if (x & 1) {
+			res ^= 1;
+		}
+		x >>= 1;
+	}
+	return res;
+}
+void solve() {
+	ll n;
+	cin >> n >> k;
+	rec(n, 0);
+	if (n & 1) {
+		cout << res * (n + 1) / 2 << endl;
+	} else {
+		ld t = (ld)(n + 1) / 2;
+		ld ans = res * t;
+		cout << (ll)(ans) << endl;
+	}
+	res = 0;
+
+	return;
+}
+
+signed main(void) {
+	std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+#ifdef LOCAL
+		cout << "--------------------------------------------" << endl;
+#endif
+	}
+
+#ifdef LOCAL
+	cerr << "Finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+	return 0;
+}
