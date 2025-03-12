2e2ec0b03220f8279e5637c56a6e993ba7d3e102
Dzanija Ivan <ivan.dzanija222@gmail.com>
2025-03-12 16:52:17 +0100

    Solved

---
+++ Codeforces/additional/2064C.cpp
@@ -0,0 +1,82 @@
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using ll = int64_t;
+using ull = uint64_t;
+using ld = long double;
+#define INF (ll)1e9
+#define all(x) (x).begin(), (x).end()
+#define MOD (ll)(1e9 + 7)
+
+using namespace __gnu_pbds;
+using namespace std;
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+template <typename T>
+using indexed_set = tree<T, null_type, std::less<T>, rb_tree_tag,
+						 tree_order_statistics_node_update>;
+struct custom_hash {
+	static ull splitmix64(ull x) {
+		// http://xorshift.di.unimi.it/splitmix64.c
+		x += 0x9e3779b97f4a7c15;
+		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+		return x ^ (x >> 31);
+	}
+
+	size_t operator()(ull x) const {
+		static const ull FIXED_RANDOM =
+			chrono::steady_clock::now().time_since_epoch().count();
+		return splitmix64(x + FIXED_RANDOM);
+	}
+};
+
+void solve() {
+	ll n;
+	cin >> n;
+	ll ans = 0;
+	vector<ll> vec(n, 0), left(n, 0), right(n, 0);
+
+	for (ll i = 0; i < n; ++i) {
+		cin >> vec[i];
+	}
+	ll l = 0, r = 0;
+	for (ll i = 0; i < n; ++i) {
+		if (vec[i] > 0) {
+			l += vec[i];
+		}
+		left[i] = l;
+	}
+	for (ll i = n - 1; i >= 0; --i) {
+		if (vec[i] < 0) {
+			r -= vec[i];
+		}
+		right[i] = r;
+	}
+	for (ll i = 0; i < n; ++i) {
+		ans = max(ans, right[i] + left[i]);
+	}
+
+	cout << ans << endl;
+
+	return;
+}
+
+signed main(void) {
+	std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+	ll tc = 1;
+	cin >> tc;
+	while (tc--) {
+		solve();
+#ifdef LOCAL
+		cout << "--------------------------------------------" << endl;
+#endif
+	}
+
+#ifdef LOCAL
+	cerr << "Finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+	return 0;
+}
