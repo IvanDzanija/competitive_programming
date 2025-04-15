e11822f99375e3edd70de5684aa3e38000c627c0
Dzanija Ivan <ivan.dzanija222@gmail.com>
2025-04-15 04:16:07 +0200

    Solved

---
+++ Codeforces/additional/1698D.cpp
@@ -0,0 +1,94 @@
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma, popcnt")
+// #pragma GCC optimization("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+#define INF (ll)1e9
+#define all(x) (x).begin(), (x).end()
+#define MOD (ll)(1e9 + 7)
+
+using namespace __gnu_pbds;
+using namespace std;
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+template <typename T>
+using indexed_set = tree<T, null_type, std::less<T>, rb_tree_tag,
+						 tree_order_statistics_node_update>;
+struct custom_hash {
+	static ull splitmix64(ull x) {
+		// http://xorshift.di.unimi.it/splitmix64.c
+		x += 0x9e3779b97f4a7c15;
+		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+		return x ^ (x >> 31);
+	}
+	size_t operator()(ull x) const {
+		static const ull FIXED_RANDOM =
+			chrono::steady_clock::now().time_since_epoch().count();
+		return splitmix64(x + FIXED_RANDOM);
+	}
+};
+void solve(void) {
+	ll n;
+	cin >> n;
+	ll low, high;
+	ll ohigh = n, olow = 1;
+	low = 1, high = (n + 1) / 2;
+	bool p = true;
+	for (int tc = 0; tc < 15; ++tc) {
+		cout << "? " << low << ' ' << high << endl;
+		cout.flush();
+		set<ll> curr;
+		for (ll i = low; i <= high; ++i) {
+			ll x;
+			cin >> x;
+			if (!(x < low || x > high)) {
+				curr.insert(x);
+			}
+		}
+		if (curr.size() & 1) {
+			if (curr.size() == 1) {
+				cout << "! " << *curr.begin() << endl;
+				return;
+			} else if (p) {
+				ohigh = high;
+				high = (high + low) / 2;
+			} else {
+				olow = low;
+				low = (high + low) / 2;
+			}
+		} else {
+			if (p) {
+				high = ohigh;
+				low = (low + high) / 2;
+			} else {
+				low = olow;
+				high = (low + high) / 2;
+			}
+			p = !p;
+		}
+	}
+	cout << low << ',' << high << endl;
+
+	return;
+}
+
+signed main(void) {
+	std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+	ll tc = 1;
+	cin >> tc;
+	while (tc--) {
+		solve();
+#ifdef LOCAL
+		cout << "--------------------------------------------" << endl;
+#endif
+	}
+
+#ifdef LOCAL
+	cerr << "Finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+	return 0;
+}
---
+++ Codeforces/additional/1994B.cpp
@@ -0,0 +1,84 @@
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma, popcnt")
+// #pragma GCC optimization("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+#define INF (ll)1e9
+#define all(x) (x).begin(), (x).end()
+#define MOD (ll)(1e9 + 7)
+
+using namespace __gnu_pbds;
+using namespace std;
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+template <typename T>
+using indexed_set = tree<T, null_type, std::less<T>, rb_tree_tag,
+						 tree_order_statistics_node_update>;
+struct custom_hash {
+	static ull splitmix64(ull x) {
+		// http://xorshift.di.unimi.it/splitmix64.c
+		x += 0x9e3779b97f4a7c15;
+		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+		return x ^ (x >> 31);
+	}
+	size_t operator()(ull x) const {
+		static const ull FIXED_RANDOM =
+			chrono::steady_clock::now().time_since_epoch().count();
+		return splitmix64(x + FIXED_RANDOM);
+	}
+};
+void solve(void) {
+	ll n;
+	cin >> n;
+	string a, b;
+	cin >> a >> b;
+	if (a[0] == '1') {
+		cout << "YES" << endl;
+	} else {
+		if (b[0] == '1') {
+			cout << "NO" << endl;
+		} else {
+			bool one = false;
+			bool zero = false;
+			for (ll i = 0; i < n; ++i) {
+				if (a[i] != b[i]) {
+					if (a[i] == '0' && !one) {
+						cout << "NO" << endl;
+						return;
+					} else if (a[i] == '1' && !zero) {
+						cout << "NO" << endl;
+						return;
+					}
+				}
+				if (a[i] == '1') {
+					one = true;
+				} else {
+					zero = true;
+				}
+			}
+			cout << "YES" << endl;
+		}
+	}
+	return;
+}
+
+signed main(void) {
+	std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+	ll tc = 1;
+	cin >> tc;
+	while (tc--) {
+		solve();
+#ifdef LOCAL
+		cout << "--------------------------------------------" << endl;
+#endif
+	}
+
+#ifdef LOCAL
+	cerr << "Finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+	return 0;
+}
