b3afff2d7ccfe9399ddefb2ac489b80a03a63bda
Dzanija Ivan <ivan.dzanija@tutamail.com>
2025-05-27 23:11:00 +0200

    auto-commit

---
+++ LeetCode/135.cpp
@@ -0,0 +1,27 @@
+// LeetCode 135. Candy
+// https://leetcode.com/problems/candy/
+
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int candy(vector<int> &ratings) {
+        int n = ratings.size();
+        vector<int> ans(n, 1);
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                ans[i] = ans[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                ans[i] = max(ans[i + 1] + 1, ans[i]);
+            }
+        }
+        cout << endl;
+
+        return accumulate(ans.begin(), ans.end(), 0);
+    }
+};
