61919555c98de9c36323255eb23ffec85868fbf9
Dzanija Ivan <ivan.dzanija@tutamail.com>
2025-06-25 03:51:16 +0200

    auto-commit

---
+++ LeetCode/002.cpp
@@ -0,0 +1,34 @@
+// LeetCode 2. Add Two Numbers
+// https://leetcode.com/problems/add-two-numbers/
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+  public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *ans = new ListNode();
+        ListNode *current = ans;
+        int carry = 0;
+        for (; l1 != nullptr || l2 != nullptr || carry > 0;) {
+            int temp = carry;
+            if (l1 != nullptr) {
+                temp += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                temp += l2->val;
+                l2 = l2->next;
+            }
+            carry = temp / 10;
+            current->next = new ListNode(temp % 10);
+            current = current->next;
+        }
+        return ans->next;
+    }
+};
