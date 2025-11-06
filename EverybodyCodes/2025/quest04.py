4a572cd1445e7fef528d158033b78b6446ec5f3e
Dzanija Ivan <ivan.dzanija@tutamail.com>
2025-11-07 00:25:04 +0100

    auto-commit

--- EverybodyCodes/2025/quest04.py
+++ EverybodyCodes/2025/quest04.py
@@ -2,25 +2,48 @@ import sys
 
 
 def part1(a):
-    ans = 0
+    ans = 2025
+    prev = a[0]
+    for i in range(1, len(a)):
+        curr = prev / a[i]
+        prev = a[i]
+        ans *= curr
+    print(ans)
 
 
 def part2(a):
-    ans = 0
+    ans = 1
+    c = 10000000000000
+    prev = a[0]
+    for i in range(1, len(a)):
+        curr = prev / a[i]
+        prev = a[i]
+        ans *= curr
+    print(c / ans)
 
 
 def part3(a):
-    ans = 0
-    print(ans)
+    ans = 100
+    prev = int(a[0])
+    for i in range(1, len(a)):
+        d = list(map(int, a[i].split("|")))
+        if len(d) == 1:
+            ans = ans * prev / int(a[i])
+        else:
+            ans = ans * prev / d[0]
+            prev = d[1]
+
+    print(int(ans))
 
 
 def main():
     infile = sys.argv[1] if len(sys.argv) >= 2 else "a.in"
     G = open(infile).read().strip()
-    G = list(map(int, G.split(",")))
-    part1(G)
-    part2(G)
-    part3(G)
+    # G = list(map(int, G.split("\n")))
+    # G = G.split("\n")
+    # part1(G)
+    # part2(G)
+    # part3(G)
 
 
 if __name__ == "__main__":
