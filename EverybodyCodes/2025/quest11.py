0a02e5aa0c044aa6911ab853fa3be6c36bd2c255
Dzanija Ivan <ivan.dzanija@tutamail.com>
2025-11-16 00:03:48 +0100

    auto-commit

---
+++ EverybodyCodes/2025/quest11.py
@@ -0,0 +1,15 @@
+import sys
+
+def part1(a):
+
+
+def main():
+    infile = sys.argv[1] if len(sys.argv) >= 2 else "a.in"
+    G = open(infile).read().strip()
+    # part1(G)
+    # part2(G)
+    # part3(G)
+
+
+if __name__ == "__main__":
+    main()
