2dad5c9e4c8df6003f9b29dfb156c2aec1a02a28
Dzanija Ivan <ivan.dzanija@tutamail.com>
2025-11-19 00:29:16 +0100

    auto-commit

--- templates/template.py
+++ templates/template.py
@@ -6,15 +6,22 @@ import sys
 from dataclasses import dataclass
 import re
 from builtins import pow
-from heapq import heappush, heappop, heappushpop, heapify, heapreplace
+
+GRID_MOVES = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+
+def part1(a):
+    ans = 0
+    print(ans)
 
 
 def main():
     infile = sys.argv[1] if len(sys.argv) >= 2 else "A.in"
     G = open(infile).read().strip()
     G = G.split("\n")
-    R = len(G)
-    C = len(G[0])
+    part1(G)
+    # part2(G)
+    # part3(G)
     return 0
 
 
