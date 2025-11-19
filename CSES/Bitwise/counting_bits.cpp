e3d88c44ec7aa95a1196484332daff947dfdb22c
Dzanija Ivan <ivan.dzanija@tutamail.com>
2025-11-19 21:07:03 +0100

    auto-commit

---
+++ CSES/Bitwise/counting_bits.cpp
@@ -0,0 +1,67 @@
+// #pragma GCC target("avx2,bmi,bmi2,fmi,lzcnt,popcnt")
+#include <bit>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+#define INF (ll)1e9
+#define all(x) (x).begin(), (x).end()
+#define MOD (ll)(1e9 + 7)
+
+using namespace __gnu_pbds;
+using namespace std;
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+template <typename T>
+using indexed_set = tree<T, null_type, std::less<T>, rb_tree_tag,
+                         tree_order_statistics_node_update>;
+struct custom_hash {
+    static ull splitmix64(ull x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(ull x) const {
+        static const ull FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+void solve(void) {
+    ll n;
+    cin >> n;
+    ll bits = 64 - countl_zero((ull)n);
+    ll ans = 0;
+
+    for (int i = 1; i <= bits; ++i) {
+        ll full = (n + 1) >> i;
+        ans += full << (i - 1);
+
+        ll rem = (n + 1) & ((1LL << (i)) - 1); // (n+1) % 2^i
+        if (rem >= (1LL << (i - 1))) {
+            ans += rem - (1LL << (i - 1));
+        }
+    }
+    cout << ans << endl;
+    return;
+}
+
+signed main(void) {
+    std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--) {
+        solve();
+#ifdef LOCAL
+        cout << "--------------------------------------------" << endl;
+#endif
+    }
+
+#ifdef LOCAL
+    cerr << "Finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+    return 0;
+}
