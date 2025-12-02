5809dcda8147dbe4c71c1e74e21e0f15cd9058a7
Dzanija Ivan <ivan.dzanija@tutamail.com>
2025-12-02 17:22:45 +0100

    auto-commit

--- AdventOfCode/2024/Day01/01.cpp
+++ AdventOfCode/2024/Day01/01.cpp
@@ -13,42 +13,42 @@ using namespace __gnu_pbds;
 using namespace std;
 template <typename T>
 using indexed_set = tree<T, null_type, std::less<T>, rb_tree_tag,
-						 tree_order_statistics_node_update>;
+                         tree_order_statistics_node_update>;
 struct custom_hash {
-	static uint64_t splitmix64(uint64_t x) {
-		// http://xorshift.di.unimi.it/splitmix64.c
-		x += 0x9e3779b97f4a7c15;
-		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-		return x ^ (x >> 31);
-	}
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
 
-	size_t operator()(uint64_t x) const {
-		static const uint64_t FIXED_RANDOM =
-			chrono::steady_clock::now().time_since_epoch().count();
-		return splitmix64(x + FIXED_RANDOM);
-	}
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
 };
 
 int main(void) {
-	std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
-	std::ifstream input("../input.txt");
-	string line;
-	vector<ll> a;
-	vector<ll> b;
-	while (getline(input, line)) {
-		a.push_back(stoll(line.substr(0, line.find(' '))));
-		line = line.substr(line.find(' ') + 1);
-		b.push_back(stoll(line));
-	}
-	sort(all(a));
-	sort(all(b));
-	ll ans = 0;
-	ll n = (ll)a.size();
-	for (ll i = 0; i < n; ++i) {
-		// cout << a[i] << ' ' << b[i] << endl;
-		ans += abs(a[i] - b[i]);
-	}
-	cout << ans << endl;
-	return 0;
+    std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+    std::ifstream input("../input.txt");
+    string line;
+    vector<ll> a;
+    vector<ll> b;
+    while (getline(input, line)) {
+        a.push_back(stoll(line.substr(0, line.find(' '))));
+        line = line.substr(line.find(' ') + 1);
+        b.push_back(stoll(line));
+    }
+    sort(all(a));
+    sort(all(b));
+    ll ans = 0;
+    ll n = (ll)a.size();
+    for (ll i = 0; i < n; ++i) {
+        // cout << a[i] << ' ' << b[i] << endl;
+        ans += abs(a[i] - b[i]);
+    }
+    cout << ans << endl;
+    return 0;
 }
